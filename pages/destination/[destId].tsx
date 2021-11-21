c677a909f6f2079f4d131895cab3609d637a63ea
avatarfreak <avatarfreak83@gmail.com>
2021-11-21 14:53:21 +0530

    Destination page dynamic routing

---
+++ pages/destination/[destId].tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { DestIProps, DestPage } from '@pages/DestPage';
+import dataJSON from '../../data.json';
+
+
+const Destination: React.FC<DestIProps> = ({ data }) => {
+
+   return <DestPage data={data} />
+}
+
+export default Destination
+
+// Static Path
+export const getStaticPaths: GetStaticPaths = async () => {
+   const paths = dataJSON.destinations.map(destination => ({
+      params: {
+         destId: destination.name.toString().toLowerCase()
+      }
+   }))
+   return { paths, fallback: false }
+}
+
+
+// Static Props
+export const getStaticProps: GetStaticProps = async (context) => {
+   const { destId } = context.params;
+
+   const destination = dataJSON.destinations.find(destination => destination.name.toString().toLowerCase() === destId);
+   return {
+      props: {
+         data: destination
+      }
+   }
+}
