3973484ca4d13830ea994ab16006d5bcd1c7dbb7
avatarfreak <avatarfreak83@gmail.com>
2021-11-22 16:45:48 +0530

    stop animation functionality added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,26 @@ import "../src/components/styles/index.scss";
 import "tailwindcss/tailwind.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+
+   // Stop animation on browser resize
+   React.useEffect(() => {
+      let resizeTimer: any;
+      window.addEventListener("resize", () => {
+         document.body.classList.add("resize-animation-stopper");
+
+         clearTimeout(resizeTimer);
+
+         resizeTimer = setTimeout(() => {
+            document.body.classList.remove("resize-animation-stopper");
+         }, 400)
+      })
+
+   }, [])
+
    return (
       <>
          <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png" />
          </Head>
          <Layout>
             <Component {...pageProps} />
