3c39d33e4c1a5323ce158f70279e6a42d043de9a
avatarfreak <avatarfreak83@gmail.com>
2021-11-22 16:16:29 +0530

    Added basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
    trailingSlash: true,
    webpackDevMiddleware: config => {
       config.watchOptions = {
